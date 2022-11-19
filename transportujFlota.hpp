5bd9d96b0e34d4d0d770148525a5b4bd37659296
Andrzej Ładomirski <andrzejladomirski@gmail.com>
2022-11-19 15:03:14 +0100

    Zadziała?

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,22 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    unsigned int licznik_zaglowcow = 0;
+    unsigned int przewieziony_towar = 0;
+
+    Stocznia stocznia1{};
+
+    while (przewieziony_towar < towar) {
+    
+        Statek* statek1 = stocznia1();
+        if (dynamic_cast< Zaglowiec* >(statek1) != nullptr) 
+        {
+            licznik_zaglowcow++;
+        }
+        przewieziony_towar += statek1->transportuj();
+
+        delete statek1;
+    }
+
+    return licznik_zaglowcow;
 }
